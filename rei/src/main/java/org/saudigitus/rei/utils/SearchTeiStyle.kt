45b07751e65fac9bb86b21c558a8b0b6b75f783c
Carlos Macaneta <56470814+CarlosMacaneta@users.noreply.github.com>
2024-10-15 09:39:43 +0200

    chore: updating the color tones of TEIs (#10)

--- rei/src/main/java/org/saudigitus/rei/utils/SearchTeiStyle.kt
+++ rei/src/main/java/org/saudigitus/rei/utils/SearchTeiStyle.kt
@@ -4,6 +4,9 @@ import androidx.compose.ui.graphics.Color
 import org.dhis2.commons.data.SearchTeiModel
 import org.hisp.dhis.android.core.D2
 import org.hisp.dhis.android.core.event.EventStatus
+import org.saudigitus.rei.ui.theme.Light_Error
+import org.saudigitus.rei.ui.theme.Light_Success
+import org.saudigitus.rei.ui.theme.Light_Warning
 
 class SearchTeiStyle(private val d2: D2) {
     fun getTeiCardBackground(searchTeiModel: SearchTeiModel): Pair<String, Color> {
@@ -15,13 +18,13 @@ class SearchTeiStyle(private val d2: D2) {
                 .one().blockingGet()
 
             when (event?.status()) {
-                EventStatus.COMPLETED -> Pair(EventStatus.COMPLETED.name.capital(), Color.Green)
-                EventStatus.OVERDUE -> Pair(EventStatus.OVERDUE.name.capital(), Color.Yellow)
-                EventStatus.SCHEDULE -> Pair(EventStatus.ACTIVE.name.capital(), Color.Red)
-                else -> Pair(EventStatus.ACTIVE.name.capital(), Color.LightGray)
+                EventStatus.COMPLETED -> Pair(EventStatus.COMPLETED.name.capital(), Light_Success.copy(.2f))
+                EventStatus.OVERDUE -> Pair(EventStatus.OVERDUE.name.capital(), Light_Warning.copy(.2f))
+                EventStatus.SCHEDULE -> Pair(EventStatus.ACTIVE.name.capital(), Light_Error.copy(.2f))
+                else -> Pair(EventStatus.ACTIVE.name.capital(), Color.Transparent)
             }
         } else {
-            Pair(EventStatus.ACTIVE.name.capital(), Color.LightGray)
+            Pair(EventStatus.ACTIVE.name.capital(), Color.Transparent)
         }
     }
 }
