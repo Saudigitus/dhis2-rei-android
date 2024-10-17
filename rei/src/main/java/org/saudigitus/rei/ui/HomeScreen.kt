c202ab087ff65e2ae87181fc8734a459cf82de6d
Carlos Macaneta <carlosmacaneta95@gmail.com>
2024-10-17 14:32:58 +0200

    chore: removing container color from FAB

--- rei/src/main/java/org/saudigitus/rei/ui/HomeScreen.kt
+++ rei/src/main/java/org/saudigitus/rei/ui/HomeScreen.kt
@@ -81,7 +81,6 @@ fun HomeScreen(
                     )
                 },
                 onClick = { },
-                containerColor = seed,
                 contentColor = Color.White,
             )
         },
